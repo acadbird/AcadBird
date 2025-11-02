f66df9e37adde7734a4678e410f853b10368de80
acadbird <acadbird@gmail.com>
2025-11-02 08:22:54 +0800

    Initial Commit

---
+++ main/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7gwN-3GVlJ-a_MTBxqVnc7Oltq5wSvHQ",
+  authDomain: "acadbird-379e3.firebaseapp.com",
+  projectId: "acadbird-379e3",
+  storageBucket: "acadbird-379e3.appspot.com",
+  messagingSenderId: "575491576951",
+  appId: "1:575491576951:web:70f86aba1e33b871f8a272",
+  measurementId: "G-N6Z672SXWJ",
+};
